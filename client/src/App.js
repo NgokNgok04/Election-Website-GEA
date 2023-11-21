3ea3feb6428468393aaeed8d0dc8295f52703d76
unknown <117185466+NgokNgok04@users.noreply.github.com>
2023-11-22 03:41:40 +0700

    build: project structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import VotingBPH from './VotingBPH';
+import VotingSenator from './VotingSenator';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className='App'>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/VotingBPH" element={<VotingBPH />}/>
+            <Route path="/VotingSenator" element={<VotingSenator />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
