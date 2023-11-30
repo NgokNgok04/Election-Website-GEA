a9a77097a2a26e45b995d19756067fd0af8353ea
unknown <117185466+NgokNgok04@users.noreply.github.com>
2023-12-01 04:47:18 +0700

    fix: home page done

--- clients/app/components/playbutton.tsx
+++ clients/app/components/playbutton.tsx
@@ -1,44 +1,42 @@
 import React from 'react'
+import Swal from 'sweetalert2';
 const playbutton = () => {
+    async function handleClick(){
+        const { value: password } = await Swal.fire({
+            position: 'bottom',
+            title: "Enter your password",
+            input: "password",
+            inputLabel: "Password",
+            inputPlaceholder: "Enter your password",
+            inputAttributes: {
+              maxlength: "10",
+              autocapitalize: "off",
+              autocorrect: "off"
+            }
+          });
+    }
   return (
-    <div>
-        <button 
-            className='
-            bg-white
-            rounded-md
-            py-2 md:py-2
-            px-6 md:px-6
-            w-auto
-            text-lg
-            font-semibold
-            flex
-            flex-row
-            item-center
-            hover:bg-neutral-300
-            transition
-            align-middle
-        '>
-            <img src='../images/play-button.png' className='mr-1 h-6' /> 
-            <p className='align-middle'> Play </p>
-        </button>
-    </div>
+    <button onClick={handleClick}
+        className='
+        bg-white
+        rounded-md
+        py-2 md:py-2
+        px-6 md:px-6
+        w-auto
+        text-lg
+        font-semibold
+        flex
+        flex-row
+        item-center
+        hover:bg-neutral-300
+        transition
+        align-middle
+    '>
+        <img src='../images/play-button.png' className='mr-1 h-6' /> 
+        <p className='align-middle'> Play </p>
+        
+    </button>
   )
 }
 
 export default playbutton
-
-{/* <button className='
-mt-3
-text-4xl
-bg-white
-rounded-xl
-font-medium
-tracking-normal
-px-5
-o
-font-sans
-justify'>
-  <img src='/images/play-button.png'
-  className='h-8 flex '/>
-   Play 
-   </button> */}
