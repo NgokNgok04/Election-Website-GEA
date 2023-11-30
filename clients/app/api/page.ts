9edc721e4e0566a4618266b515db4eb849b8a80a
Ignatius Jhon Hezkiel Chan <kielchanyeol@gmail.com>
2023-12-01 01:52:57 +0700

    feat: add schema prisma

---
+++ clients/app/api/page.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {prisma} from "../../lib/prisma"
+export default async function handle(req:NextApiRequest,res:NextApiResponse) {
+
+    if(req.method === "POST"){
+        const result = await prisma.
+    }
+    
+    return res.status(200).json({data:"oke"})
+}
