b8cf15fddfd9c06c5b8316282cc1e05825b210d6
Berto Richardo Togatorop <bertotogatorop27@gmail.com>
2023-12-02 01:51:59 +0700

    feat: add title and onclick effect on imageCard

--- clients/app/components/ImageCard.tsx
+++ clients/app/components/ImageCard.tsx
@@ -10,23 +10,25 @@ interface ImageCardProps {
 
 const ImageCard: React.FC<ImageCardProps> = ({ src, nama, nim, handleClick, selected }) => {
   return (
-    <div
-      onClick={handleClick}
-      className={`max-w-sm flex-shrink bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:transition-transform hover:scale-105 hover:cursor-pointer ${
-        selected ? 'border-blue-500' : ''
-      }`}
-    >
-      <img className="rounded-t-lg flex-shrink" src={src} alt="" />
+    <div>
+      <button
+        onClick={handleClick}
+        className={`max-w-sm flex-shrink rounded-lg shadow bg-gray-800 hover:transition-transform 
+          transition duration-150 ease-in-out hover:scale-105 hover:cursor-pointer'
+          ${selected ? 'border-2 border-red-400 transition-all duration-150 ease-in-out ' : 'border border-transparent'}`}
+      >
+        <img className="rounded-t-lg flex-shrink" src={src} alt="" />
 
-      <div className="p-5 text-center">
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{nama}</p>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{nim}</p>
-        {selected ? (
-          <div className="text-green-500 font-semibold mb-2">Selected</div>
-        ) : (
-          <div className="text-gray-500 mb-2">Not Selected</div>
-        )}
-      </div>
+        <div className="p-5 text-center">
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{nama}</p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{nim}</p>
+          {selected ? (
+            <div className="text-green-500 font-semibold mb-2">Selected</div>
+          ) : (
+            <div className="text-gray-500 mb-2">Not Selected</div>
+          )}
+        </div>
+      </button>
     </div>
   );
 };
